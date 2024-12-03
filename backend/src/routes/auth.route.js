bf08ecb99704772aec9a218492f34bbebd9acf4c
Yen-Chun,Lin <asd900606@gmail.com>
2024-12-03 16:24:53 +0800

    feat: check auth

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -4,8 +4,9 @@ import {
   login,
   logout,
   updateProfile,
+  checkAuth,
 } from "../controllers/auth.controller.js";
-import protectRoute from "../middleware/auth.middleware.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
@@ -16,5 +17,7 @@ router.post("/login", login);
 router.post("/logout", logout);
 
 router.put("/update-profile", protectRoute, updateProfile);
+
+router.get("/check", protectRoute, checkAuth);
 
 export default router;
