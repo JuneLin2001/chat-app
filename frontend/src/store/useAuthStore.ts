ddd8c1d56eb772e9c71387dc48c19a3672822558
Yen-Chun,Lin <asd900606@gmail.com>
2024-12-05 04:17:43 +0800

    feat: add signUp and logout

--- frontend/src/store/useAuthStore.ts
+++ frontend/src/store/useAuthStore.ts
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axios";
+import toast from "react-hot-toast";
 
 interface AuthState {
   authUser: { id: string; name: string } | null;
@@ -8,6 +9,8 @@ interface AuthState {
   isUpdatingProfile: boolean;
   isCheckingAuth: boolean;
   checkAuth: () => Promise<void>;
+  signUp: (data: any) => Promise<void>;
+  logout: () => Promise<void>;
 }
 
 export const useAuthStore = create<AuthState>((set) => ({
@@ -26,6 +29,29 @@ export const useAuthStore = create<AuthState>((set) => ({
       set({ authUser: null });
     } finally {
       set({ isCheckingAuth: false });
+    }
+  },
+
+  signUp: async (data) => {
+    try {
+      const res = await axiosInstance.post("/auth/signup", data);
+      set({ authUser: res.data });
+      toast.success("Signup successful");
+    } catch (error) {
+      console.log("Error in signup: ", error);
+    } finally {
+      set({ isSigningUp: false });
+    }
+  },
+
+  logout: async () => {
+    try {
+      await axiosInstance.post("/auth/logout");
+      set({ authUser: null });
+      toast.success("Logout successful");
+    } catch (error) {
+      console.log("Error in logout: ", error);
+      toast.error("Logout failed");
     }
   },
 }));
