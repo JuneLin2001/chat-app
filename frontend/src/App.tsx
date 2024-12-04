4d6a7a499aa3b5bf8d9014207e6c80c700665c2e
Yen-Chun,Lin <asd900606@gmail.com>
2024-12-05 02:37:54 +0800

    chore: set routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,22 @@
+import Navbar from "./components/Navbar.tsx";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage.tsx";
+import SignUpPage from "./pages/SignUpPage.tsx";
+import LoginPage from "./pages/LoginPage.tsx";
+import Settingage from "./pages/Settingage.tsx";
+import ProfilePage from "./pages/ProfilePage.tsx";
+
 const App = () => {
   return (
     <>
-      <button className="btn">Button</button>
-      <button className="btn btn-neutral">Neutral</button>
-      <button className="btn btn-primary">Primary</button>
-      <button className="btn btn-secondary">Secondary</button>
-      <button className="btn btn-accent">Accent</button>
-      <button className="btn btn-ghost">Ghost</button>
-      <button className="btn btn-link">Link</button>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/settings" element={<Settingage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
     </>
   );
 };
---
+++ frontend/src/pages/Settingage.tsx
@@ -0,0 +1,5 @@
+const Settingage = () => {
+  return <div>Settingage</div>;
+};
+
+export default Settingage;
