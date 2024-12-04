4ccfb6fdc3c40d30e279df16bf6598e42072a9b2
Yen-Chun,Lin <asd900606@gmail.com>
2024-12-05 03:04:39 +0800

    Create useAuthStore.ts

---
+++ frontend/src/store/useAuthStore.ts
@@ -0,0 +1,31 @@
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios";
+
+interface AuthState {
+  authUser: { id: string; name: string } | null;
+  isSigningUp: boolean;
+  isLoggingIn: boolean;
+  isUpdatingProfile: boolean;
+  isCheckingAuth: boolean;
+  checkAuth: () => Promise<void>;
+}
+
+export const useAuthStore = create<AuthState>((set) => ({
+  authUser: null,
+  isSigningUp: false,
+  isLoggingIn: false,
+  isUpdatingProfile: false,
+  isCheckingAuth: true,
+
+  checkAuth: async () => {
+    try {
+      const res = await axiosInstance.get("/auth/check");
+      set({ authUser: res.data });
+    } catch (error) {
+      console.log("Error in checkAuth: ", error);
+      set({ authUser: null });
+    } finally {
+      set({ isCheckingAuth: false });
+    }
+  },
+}));
