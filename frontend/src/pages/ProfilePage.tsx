cfa9ca471b4851b3593f511e793da42a17697607
Yen-Chun,Lin <asd900606@gmail.com>
2024-12-05 04:54:27 +0800

    fix: add if !authUser

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -23,6 +23,10 @@ const ProfilePage = () => {
     };
   };
 
+  if (!authUser) {
+    return null;
+  }
+
   return (
     <div className="h-screen pt-20">
       <div className="max-w-2xl mx-auto p-4 py-8">
@@ -35,7 +39,7 @@ const ProfilePage = () => {
           <div className="flex flex-col items-center gap-4">
             <div className="relative">
               <img
-                src={selectedImg || (authUser?.profilePic ?? "/avatar.png")}
+                src={selectedImg || authUser.profilePic || "/avatar.png"}
                 alt="Profile"
                 className="size-32 rounded-full object-cover border-4 "
               />
