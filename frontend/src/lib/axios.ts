e5b040751053621ab999e4e9fffa86f9aa4cf080
Yen-Chun,Lin <asd900606@gmail.com>
2024-12-05 09:13:57 +0800

    chore: set deploy environment

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,6 +1,7 @@
 import axio from "axios";
 
 export const axiosInstance = axio.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL:
+    import.meta.env.MODE === "development" ? "http://localhost:5001" : "/api",
   withCredentials: true,
 });
--- frontend/src/store/useAuthStore.ts
+++ frontend/src/store/useAuthStore.ts
@@ -3,7 +3,8 @@ import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
 import { Socket, io } from "socket.io-client";
 
-const BASE_URL = "http://localhost:5001";
+const BASE_URL =
+  import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
 
 type AuthState = {
   authUser: {
